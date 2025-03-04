a79d33ab56fba79ece04def9f90996c1a3902d1c
Sssrajputt <149288600+Sssrajputt@users.noreply.github.com>
2025-03-04 11:00:26 +0530

    [Shubham Rajput] Added : UC16

--- employee-payroll-app/src/main/java/com/bridgelabz/employee_payroll_app/service/EmployeeService.java
+++ employee-payroll-app/src/main/java/com/bridgelabz/employee_payroll_app/service/EmployeeService.java
@@ -3,24 +3,28 @@ package com.bridgelabz.employee_payroll_app.service;
 import com.bridgelabz.employee_payroll_app.dto.EmployeeDTO;
 import com.bridgelabz.employee_payroll_app.exception.EmployeeNotFoundException;
 import com.bridgelabz.employee_payroll_app.model.Employee;
+import com.bridgelabz.employee_payroll_app.repository.EmployeeRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 @Slf4j
 public class EmployeeService implements IEmployeeService {
-    private final List<Employee> employeeList = new ArrayList<>();
-    private final AtomicLong idCounter = new AtomicLong();
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     @Override
     public Employee createEmployee(EmployeeDTO employeeDTO) {
         Employee employee = new Employee();
-        employee.setId(idCounter.incrementAndGet());
         employee.setName(employeeDTO.getName());
         employee.setSalary(employeeDTO.getSalary());
         employee.setGender(employeeDTO.getGender());
@@ -28,21 +32,20 @@ public class EmployeeService implements IEmployeeService {
         employee.setNote(employeeDTO.getNote());
         employee.setProfilePic(employeeDTO.getProfilePic());
         employee.setDepartment(employeeDTO.getDepartment());
-        employeeList.add(employee);
-        log.info("Created employee with ID: " + employee.getId());
-        return employee;
+        log.info("Created employee with name: " + employee.getName());
+        return employeeRepository.save(employee);
     }
 
     @Override
     public Optional<Employee> getEmployeeById(Long id) {
         log.info("Fetching employee with ID: " + id);
-        return employeeList.stream().filter(e -> e.getId().equals(id)).findFirst();
+        return employeeRepository.findById(id);
     }
 
     @Override
     public List<Employee> getAllEmployees() {
         log.info("Fetching all employees");
-        return new ArrayList<>(employeeList);
+        return employeeRepository.findAll();
     }
 
     @Override
@@ -56,15 +59,13 @@ public class EmployeeService implements IEmployeeService {
         employee.setProfilePic(employeeDTO.getProfilePic());
         employee.setDepartment(employeeDTO.getDepartment());
         log.info("Updated employee with ID: " + id);
-        return employee;
+        return employeeRepository.save(employee);
     }
 
     @Override
     public void deleteEmployee(Long id) {
-        if (!employeeList.removeIf(employee -> employee.getId().equals(id))) {
-            log.error("Employee with ID " + id + " not found");
-            throw new EmployeeNotFoundException("Employee with ID " + id + " not found");
-        }
+        Employee employee = getEmployeeById(id).orElseThrow(() -> new EmployeeNotFoundException("Employee with ID " + id + " not found"));
+        employeeRepository.delete(employee);
         log.info("Deleted employee with ID: " + id);
     }
 }
